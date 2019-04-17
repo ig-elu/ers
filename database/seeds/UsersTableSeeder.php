5d67be231b3fcf71502d147dd1667b50de0017d0
madehmlow <madehmlow@gmail.com>
2019-04-17 17:18:53 -0400

    first commit, vanilla laravel with authentication

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@app.local',
+            'username' => 'admin',
+            'password' => bcrypt('secret'),
+        ]);    
+    }
+}
